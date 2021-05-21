fac104d3008ddf254da76899cb7c502c6b399328
MZiserman <martinziserman@gmail.com>
2021-05-21 12:06:35 +0200

    fix realistic fees

--- lib/blend_spreadsheet_loan_generator/realistic_interests.rb
+++ lib/blend_spreadsheet_loan_generator/realistic_interests.rb
@@ -11,8 +11,7 @@ module BlendSpreadsheetLoanGenerator
       "=#{excel_float(loan.rate)} * ((#{period_leap_days(line)} / 366) + (#{period_non_leap_days(line)} / 365))"
     end
 
-
-    def period_fees_rate_formula(*)
+    def period_fees_rate_formula(line:)
       "=#{excel_float(loan.fees_rate)} * ((#{period_leap_days(line)} / 366) + (#{period_non_leap_days(line)} / 365))"
     end
   end
